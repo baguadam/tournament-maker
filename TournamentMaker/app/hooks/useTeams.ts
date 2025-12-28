f577d364e5be89533c11c6e9057e86208b8198c2
baguadam <m3wdo5@inf.elte.hu>
2025-12-28 18:56:51 +0100

    Refactor naming in useTeams hook

--- TournamentMaker/app/hooks/useTeams.ts
+++ TournamentMaker/app/hooks/useTeams.ts
@@ -3,24 +3,24 @@ import teamsData from "../../data/teams.json";
 import { Team, Teams } from "../types";
 
 const useTeams = () => {
-  const [teams, setTeams] = useState<Teams>(teamsData);
+  const [teamsObject, setTeamsObject] = useState<Teams>(teamsData);
 
   // operations
   const addTeam = (team: Team) => {
-    setTeams({ teams: [...teams.teams, team] });
+    setTeamsObject({ teams: [...teamsObject.teams, team] });
   };
 
   const addPlayer = (teamId: string, player: string) => {
-    const newTeams: Team[] = teams.teams.map((team) =>
+    const newTeams: Team[] = teamsObject.teams.map((team) =>
       team.id === teamId
         ? { ...team, players: [...team.players, player] }
         : team
     );
 
-    setTeams({ teams: newTeams });
+    setTeamsObject({ teams: newTeams });
   };
 
-  return { teams, addTeam, addPlayer };
+  return { teamsObject, addTeam, addPlayer };
 };
 
 export default useTeams;
